c3cb12e50bd98b2b2adefcd9c71744e2f64fa184
Dileep <kadileepjee@gmail.com>
2023-08-20 18:47:52 +0530

    updated registration form

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,203 +1,47 @@
-import { UserCircleIcon } from '@heroicons/react/24/solid';
-import Link from 'next/link';
-import SaveBtn from '../../components/SaveBtn';
+import { UserCircleIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import SaveBtn from "../../components/SaveBtn";
 
 export default function SignUp() {
   return (
     <main>
-      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8" style={{backgroundImage: "url('assets/Images/PurpleAbstract/PurpleHaze.jpg')"}}>
-        <form>
-          <div className="space-y-12">
-            <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-xl font-semibold leading-7 text-gray-900">Personal Information</h2>
-              <p className="mt-1 text-lg leading-6 text-gray-600">Enter a unique username and password.</p>
-            
-
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:col-span-4">
-                  <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                    Username
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      <input
-                        type="text"
-                        name="username"
-                        id="username"
-                        autoComplete="username"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="janesmith"
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="sm:col-span-4">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Create a password"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
-              <p className="mt-1 text-sm leading-6 text-gray-600">
-                This information will be displayed publicly so be careful what you share.
-              </p>
-
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:col-span-3">
-                  <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                    First name
-                  </label>
-                  <div className="mt-2">
+      <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center mt-10">
+        <div className="container max-w-screen-lg mx-auto">
+          <div>
+            <h2 className="font-semibold text-xl text-gray-600">
+              Registration Form
+            </h2>
+            <p className="text-gray-500 mb-6 font-medium">
+              Your Experience Matters: Join the Conversation and Share Your
+              Journey!
+            </p>
+            <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+              <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+                <div className="mb-5">
+                  <div className="md:col-span-2 mb-6">
+                    <label htmlFor="username">User Name</label>
                     <input
                       type="text"
-                      name="first-name"
-                      id="first-name"
-                      autoComplete="given-name"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      name="username"
+                      id="username"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      defaultValue=""
+                      placeholder="Janesmith"
+                      autoComplete="username"
                     />
                   </div>
-                </div>
-
-                <div className="sm:col-span-3">
-                  <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Last name
-                  </label>
-                  <div className="mt-2">
+                  <div className="md:col-span-2 mb-6">
+                    <label htmlFor="password">Password</label>
                     <input
-                      type="text"
-                      name="last-name"
-                      id="last-name"
-                      autoComplete="family-name"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      type="password"
+                      name="password"
+                      id="password"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      defaultValue=""
+                      placeholder="Create a password"
                     />
                   </div>
-                </div>
-
-                <div className="sm:col-span-4">
-                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                    Email address
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="col-span-full">
-                  <label htmlFor="gender" className="block text-sm font-medium leading-6 text-gray-900">
-                    Gender
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="gender"
-                      id="gender"
-                      autoComplete="street-address"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2 sm:col-start-1">
-                  <label htmlFor="ethnicity" className="block text-sm font-medium leading-6 text-gray-900">
-                    Ethnicity
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="ethnicity"
-                      id="ethnicity"
-                      autoComplete="address-level2"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2">
-                  <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
-                    Country
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="country"
-                      id="country"
-                      autoComplete="address-level1"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2">
-                  <label htmlFor="phone" className="block text-sm font-medium leading-6 text-gray-900">
-                    Phone number
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="tel"
-                      name="phone"
-                      id="phone"
-                      autoComplete="phone"
-                      placeholder="123-45-678" 
-                      pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2">
-                  <label htmlFor="url" className="block text-sm font-medium leading-6 text-gray-900">
-                    Socials link
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="url"
-                      name="url"
-                      id="url"
-                      placeholder="https://example.com" pattern="https://.*" 
-                      size="30"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="col-span-full">
-                  <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                    About
-                  </label>
-                  <div className="mt-2">
-                    <textarea
-                      id="about"
-                      name="about"
-                      rows={3}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      defaultValue={''}
-                    />
-                  </div>
-                  <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-                </div>
-
-                <div className="col-span-full">
+                  <div className="col-span-full">
                   <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
                     Photo
                   </label>
@@ -211,23 +55,158 @@ export default function SignUp() {
                     </button>
                   </div>
                 </div>
+                </div>
+                <div className="lg:col-span-2">
+                  <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+                    <div className="md:col-span-3 mb-4">
+                      <label htmlFor="first-name">First Name</label>
+                      <input
+                        type="text"
+                        name="first-name"
+                        id="first-name"
+                        className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        autoComplete="given-name"
+                      />
+                    </div>
+                    <div className="md:col-span-2 mb-4">
+                      <label htmlFor="last-name">Last Name</label>
+                      <input
+                        type="text"
+                        name="last-name"
+                        id="last-name"
+                        className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        autoComplete="family-name"
+                      />
+                    </div>
+                    <div className="md:col-span-5 mb-4" >
+                      <label htmlFor="email">Email Address</label>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        placeholder="email@domain.com"
+                        autoComplete="email"
+                      />
+                    </div>
+                    <div className="md:col-span-5 mb-4">
+                      <label htmlFor="about">About You</label>
+                      <textarea
+                        id="about"
+                        name="about"
+                        rows="6"
+                        className="border mt-1 rounded px-4 w-full bg-gray-50"
+                      ></textarea>
+                    </div>
+                    <div className="md:col-span-5 text-right">
+                    <div className="inline-flex items-end">
+                      <Link href="/dashboard">
+                        <button
+                          type="button"
+                          className="bg-primary text-white font-bold py-2 px-4 rounded"
+                          data-te-ripple-init
+                          data-te-ripple-color="light"
+                        >
+                          Register
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
 
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            <Link href='/'>
-              <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-                Cancel
+      {/* NAVBAR */}
+
+      <div class="bg-white">
+        <header class="absolute inset-x-0 top-0 z-50">
+          <nav
+            class="flex items-center justify-between p-6 lg:px-8 mx-auto max-w-7xl px-4 py-7 sm:px-6"
+            aria-label="Global"
+          >
+            <div class="flex lg:flex-1">
+              <a href="/" class="-m-1.5 p-1.5">
+                <span class="sr-only">Migrant. Mingle</span>
+                {/* <img class="h-8 w-auto" src="assets/LogoDesign1/coconotLightWhiteBgd.jpg" width="200" alt="Migrant.Mingle" /> */}
+                <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                  migrant.
+                </h1>
+                <p
+                  className="tracking-tight text-gray-400"
+                  style={{ marginLeft: "65px", marginTop: "-11px" }}
+                >
+                  MINGLE
+                </p>
+              </a>
+            </div>
+            <div class="flex lg:hidden">
+              <button
+                type="button"
+                class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              >
+                <span class="sr-only">Open main menu</span>
+                <svg
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
               </button>
-            </Link>
-            
-            <Link href='/dashboard'>
-              <SaveBtn />
-            </Link>            
-          </div>
-        </form>
+            </div>
+            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+              <a href="/" class="text-l font-semibold leading-6 text-gray-900">
+                Log in <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </nav>
+
+          {/* ==== RESPONSIVE DESIGN ===== */}
+          {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
+          {/* <div class="lg:hidden" role="dialog" aria-modal="true"> */}
+          {/* <!-- Background backdrop, show/hide based on slide-over state. -->
+             <div class="fixed inset-0 z-50"></div>
+            <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div class="flex items-center justify-between">
+                <a href="#" class="-m-1.5 p-1.5">
+                  <span class="sr-only">Your Company</span>
+                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                </a>
+                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+                  <span class="sr-only">Close menu</span>
+                  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div class="mt-6 flow-root">
+                <div class="-my-6 divide-y divide-gray-500/10">
+                  <div class="space-y-2 py-6">
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Product</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+                  </div>
+                  <div class="py-6">
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> */}
+        </header>
       </div>
     </main>
-  )
+  );
 }
