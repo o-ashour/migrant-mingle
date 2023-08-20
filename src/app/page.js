023b3991d474353188ff38e1605929485da32c75
vaishali <vaishalirdj14@gmail.com>
2023-08-20 16:48:17 +0530

    modified

--- src/app/page.js
+++ src/app/page.js
@@ -3,84 +3,209 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
+      <div class="relative isolate px-6 pt-14 lg:px-8">
+
+        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+
+          {/* class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#9775FA] to-[#693FE3] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" */}
+
+          <div style={{ clipPath: "clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
+        </div>
+        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-24">
+          <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+            </div>
+          </div>
+          <div class="text-center">
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+              <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+              <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+            </div>
+          </div>
+        </div>
+        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+
+          {/* class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#693FE3] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" */}
+
+          <div style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
+        </div>
+      </div>
+
+
       <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <section className="h-screen">
           <div className="h-full">
-            <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+            <div className="g-6 flex h-full flex-wrap items-center justify-center">
               <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
-                <img
-                  src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+
+                <div class="group relative" style={{marginLeft: "-267px",width:"193%" }}>
+                  <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 sm:h-80">
+
+                    <img src="https://images.unsplash.com/photo-1522543558187-768b6df7c25c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="h-full w-full object-cover object-center" />
+
+                  </div>
+
+                  {/* <h3 class="mt-6 text-sm text-gray-500">
+                    <a href="#">
+                      <span class="absolute inset-0"></span>
+                      Self-Improvement
+                    </a>
+                  </h3> */}
+
+{/* className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12" */}
+
+                  <div>
+                    <form >
+                      <div className="my-6 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+                        <p className="mx-4 mb-0 text-center text-2xl font-semibold text-black">
+                          Sign in
+                        </p>
+                      </div>
+
+                      <div className="relative mb-6" data-te-input-wrapper-init>
+                        <input
+                          type="text"
+                          className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          id="exampleFormControlInput2"
+                          placeholder="Email address"
+                        />
+                        <label
+                          htmlFor="exampleFormControlInput2"
+                          className="pointer-events-none absolute text-xl left-3 top-0 mb-0 max-w-[90%] pt-[0.37rem] leading-[2.15] origin-[0_0] truncate transition-all duration-200 ease-out peer-focus:-translate-y-[2rem] peer-focus:text-xl peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary text-black"
+                        >
+                          Email address
+                        </label>
+                      </div>
+
+                      <div className="relative mt-10 mb-6" data-te-input-wrapper-init>
+                        <input
+                          type="password"
+                          className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          id="exampleFormControlInput22"
+                          placeholder="Password"
+                        />
+                        <label
+                          htmlFor="exampleFormControlInput22"
+                          className="pointer-events-none absolute text-xl left-3 top-0 mb-0 max-w-[90%] pt-[0.37rem] leading-[2.15] origin-[0_0] truncate transition-all duration-200 ease-out peer-focus:-translate-y-[2rem] peer-focus:text-xl peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary text-black"
+                        >
+                          Password
+                        </label>
+                      </div>
+
+                      <div className="text-center lg:text-left">
+                        <Link href="/dashboard">
+                          <button
+                            type="button"
+                            className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-base font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                            data-te-ripple-init
+                            data-te-ripple-color="light"
+                          >
+                            Login
+                          </button>
+                        </Link>
+
+                        <p className="my-3 pt-1 text-base font-semibold">
+                          Don't have an account?
+                          <Link
+                            href="/signup"
+                            className="text-[#512EB7] ml-2 transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700"
+                          >
+                            Register
+                          </Link>
+                        </p>
+                      </div>
+                    </form>
+                  </div>
+
+                </div>
+
+                {/* <img
+                  src="https://images.unsplash.com/photo-1522543558187-768b6df7c25c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
                   className="w-full"
                   alt="Sample image"
-                />
+                  style={{ borderRadius: "10px", opacity: "1", boxShadow: "rgb(69, 47, 134) 3px 5px 20px 3px" }}
+                /> */}
+                {/* // src="https://github.com/o-ashour/empower-hack/blob/e01188b60e4e53a43e95b77410bd085d19a857ab/assets/Images/CommunityBondingImage/PurpleBondingGirls.jpg" */}
+
               </div>
 
-              <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
-                <form>
-                  <div className="my-6 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-                    <p className="mx-4 mb-0 text-center text-2xl font-semibold text-black">
-                      Sign in
-                    </p>
-                  </div>
-
-                  <div className="relative mb-6" data-te-input-wrapper-init>
-                    <input
-                      type="text"
-                      className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                      id="exampleFormControlInput2"
-                      placeholder="Email address"
-                    />
-                    <label
-                      htmlFor="exampleFormControlInput2"
-                      className="pointer-events-none absolute text-xl left-3 top-0 mb-0 max-w-[90%] pt-[0.37rem] leading-[2.15] origin-[0_0] truncate transition-all duration-200 ease-out peer-focus:-translate-y-[2rem] peer-focus:text-xl peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary text-black"
-                    >
-                      Email address
-                    </label>
-                  </div>
-
-                  <div className="relative mt-10 mb-6" data-te-input-wrapper-init>
-                    <input
-                      type="password"
-                      className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                      id="exampleFormControlInput22"
-                      placeholder="Password"
-                    />
-                    <label
-                      htmlFor="exampleFormControlInput22"
-                      className="pointer-events-none absolute text-xl left-3 top-0 mb-0 max-w-[90%] pt-[0.37rem] leading-[2.15] origin-[0_0] truncate transition-all duration-200 ease-out peer-focus:-translate-y-[2rem] peer-focus:text-xl peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary text-black"
-                    >
-                      Password
-                    </label>
-                  </div>
-
-                  <div className="text-center lg:text-left">
-                    <Link href="/dashboard">
-                      <button
-                        type="button"
-                        className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-base font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                        data-te-ripple-init
-                        data-te-ripple-color="light"
-                      >
-                        Login
-                      </button>
-                    </Link>
-
-                    <p className="my-3 pt-1 text-base font-semibold">
-                      Don't have an account?
-                      <Link
-                        href="/signup"
-                        className="text-[#512EB7] ml-2 transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700"
-                      >
-                        Register
-                      </Link>
-                    </p>
-                  </div>
-                </form>
-              </div>
             </div>
           </div>
         </section>
       </div>
+
+
+      <div class="bg-white">
+        <header class="absolute inset-x-0 top-0 z-50">
+          <nav class="flex items-center justify-between p-6 lg:px-8 mx-auto max-w-7xl px-4 py-7 sm:px-6 lg:px-8" aria-label="Global">
+            <div class="flex lg:flex-1">
+              <a href="#" class="-m-1.5 p-1.5">
+                <span class="sr-only">Migrant. Mingle</span>
+                {/* <img class="h-8 w-auto" src="assets/LogoDesign1/coconotLightWhiteBgd.jpg" width="200" alt="Migrant.Mingle" /> */}
+                <h1 className="text-3xl font-bold tracking-tight text-gray-900">migrant.</h1>
+                <p className="tracking-tight text-gray-400" style={{ marginLeft: "65px", marginTop: "-11px" }}>MINGLE</p>
+
+              </a>
+            </div>
+            <div class="flex lg:hidden">
+              <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                <span class="sr-only">Open main menu</span>
+                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+              </button>
+            </div>
+            <div class="hidden lg:flex lg:gap-x-12">
+              <a href="#" class="text-l font-semibold leading-6 text-gray-900">Product</a>
+              <a href="#" class="text-l font-semibold leading-6 text-gray-900">Features</a>
+              <a href="#" class="text-l font-semibold leading-6 text-gray-900">Marketplace</a>
+              <a href="#" class="text-l font-semibold leading-6 text-gray-900">Company</a>
+            </div>
+            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+              <a href="#" class="text-l font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+            </div>
+          </nav>
+
+
+          {/* ==== RESPONSIVE DESIGN ===== */}
+          {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
+          {/* <div class="lg:hidden" role="dialog" aria-modal="true"> */}
+            {/* <!-- Background backdrop, show/hide based on slide-over state. -->
+             <div class="fixed inset-0 z-50"></div>
+            <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div class="flex items-center justify-between">
+                <a href="#" class="-m-1.5 p-1.5">
+                  <span class="sr-only">Your Company</span>
+                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                </a>
+                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+                  <span class="sr-only">Close menu</span>
+                  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div class="mt-6 flow-root">
+                <div class="-my-6 divide-y divide-gray-500/10">
+                  <div class="space-y-2 py-6">
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Product</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+                  </div>
+                  <div class="py-6">
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> */} 
+        </header>
+      </div>
+
     </main>
   );
 }
