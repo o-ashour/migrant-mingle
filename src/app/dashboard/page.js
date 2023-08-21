5428a5dfc6ff583d2af4ea0ffd982302f96de1f7
vaishali <vaishalirdj14@gmail.com>
2023-08-21 20:07:33 +0530

    modified

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,9 +3,9 @@ import Link from "next/link"
 
 export default function Dashboard() {
   return (
-    <div className=" bg-[#B2A9EC] min-h-full">
+    <div className="bg-[#D2C6F7] min-h-full">
       <header className="bg-white shadow">
-        
+       
         <div className="mx-auto max-w-7xl px-4 py-7 sm:px-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
@@ -75,12 +75,12 @@ export default function Dashboard() {
           </div>
         </div>
 
-        <div className="bg-[#B2A9EC]" id="BODY">
+        <div id="BODY">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
               <h1 className="text-5xl font-bold text-gray-900" style={{ textAlign: "center" }}>Stories around you</h1>
-              <div className="text-xl text-gray-300" style={{ textAlign: "center", marginTop: "20px" }}>Brought to you by your fellow immigrant buddies!</div>
-              <StoriesPreview className="bg-[#B2A9EC]" />
+              <div className="text-xl text-gray-700" style={{ textAlign: "center", marginTop: "20px" }}>Brought to you by your fellow immigrant buddies!</div>
+              <StoriesPreview/>
             </div>
           </div>
         </div>
